f918059e04f97257cba3e4d30524483899983b6e
ohaziki2 <haruya0014@gmail.com>
2023-07-14 19:33:04 +0900

    敗北画面

---
+++ script.js
@@ -0,0 +1,76 @@
+// モンスターのデータ
+const monsters = [
+  { name: 'モンスター1', winRate: 0.3, image: '1.png' },
+  { name: 'モンスター2', winRate: 0.4, image: '2.png' },
+  { name: 'モンスター3', winRate: 0.5, image: '3.png' },
+  { name: 'モンスター4', winRate: 0.5, image: '4.png' },
+  { name: 'モンスター5', winRate: 0.5, image: '5.png' },
+  { name: 'モンスター6', winRate: 0.5, image: '6.png' },
+  { name: 'モンスター7', winRate: 0.5, image: '7.png' },
+  { name: 'モンスター8', winRate: 0.5, image: '8.png' },
+  { name: 'モンスター9', winRate: 0.5, image: '9.png' },
+  { name: 'モンスター10', winRate: 0.5, image: '10.png' },
+  { name: 'モンスター11', winRate: 0.5, image: '11.png' },
+  { name: 'モンスター12', winRate: 0.8, image: '12.png' }
+];
+
+let currentMonster;
+let victories = 0;
+let defeats = 0;
+
+const startScreen = document.getElementById('start-screen');
+const gameScreen = document.getElementById('game-screen');
+const resultScreen = document.getElementById('result-screen');
+const resultText = document.getElementById('result-text');
+
+// スタートボタンのクリックイベント
+document.getElementById('start-button').addEventListener('click', () => {
+  startScreen.style.display = 'none';
+  gameScreen.style.display = 'block';
+  showRandomMonster();
+});
+
+// 戦闘ボタンのクリックイベント
+document.getElementById('attack-button').addEventListener('click', () => {
+  const random = Math.random();
+
+  if (random <= currentMonster.winRate) {
+    victories++;
+    defeats = 0;
+    resultText.textContent = '勝利！';
+  } else {
+    victories = 0;
+    defeats++;
+    resultText.textContent = '敗北...';
+
+    if (defeats >= 1) {
+      gameScreen.style.display = 'none';
+      resultScreen.style.display = 'block';
+      document.body.style.backgroundColor = 'black';
+      setTimeout(() => {
+        resultScreen.style.display = 'none';
+        startScreen.style.display = 'block';
+        document.body.style.backgroundColor = '';
+        defeats = 0;
+      }, 3000);
+      return;
+    }
+  }
+
+  gameScreen.style.display = 'none';
+  resultScreen.style.display = 'block';
+
+  if (victories >= 10) {
+    resultScreen.style.display = 'none';
+    document.getElementById('game-clear-screen').style.display = 'block';
+  }
+});
+
+function showRandomMonster() {
+  currentMonster = monsters[Math.floor(Math.random() * monsters.length)];
+  const monsterImage = document.getElementById('monster-image');
+  monsterImage.src = currentMonster.image;
+  monsterImage.alt = currentMonster.name;
+  document.getElementById('monster-name').textContent = currentMonster.name;
+  document.getElementById('win-rate').textContent = `勝率: ${currentMonster.winRate * 100}%`;
+}
