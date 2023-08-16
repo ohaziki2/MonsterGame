0e475df49872fab5ff1c4ccbc2bbf461baac692b
ohaziki2 <haruya0014@gmail.com>
2023-08-16 18:54:03 +0900

    変化を加えた

--- script.js
+++ script.js
@@ -3,18 +3,11 @@ const monsters = [
   { name: 'グリーンモンスター', winRate: 0.9, image: '1.png' },
   { name: 'オレンジスパイク', winRate: 0.9, image: '2.png' },
   { name: 'アクアアイ', winRate: 0.9, image: '3.png' },
-  { name: 'ピンクゴースト', winRate: 0.9, image: '4.png' },
-  { name: 'イエローウィング', winRate: 0.9, image: '5.png' },
-  { name: 'パープルスペクター', winRate: 0.9, image: '6.png' },
-  { name: 'ライムハンド', winRate: 0.5, image: '7.png' },
-  { name: 'ネイビーマインド', winRate: 0.5, image: '8.png' },
-  { name: 'レッドデビル', winRate: 0.5, image: '9.png' },
-  { name: 'ブルーヘア', winRate: 0.5, image: '10.png' },
-  { name: 'グレイ', winRate: 0.5, image: '11.png' },
-  { name: '名前をつけれるほどの特徴がない悲しきモンスター', winRate: 0.8, image: '12.png' }
+  // ... 他のモンスターデータ ...
 ];
 
-let currentMonster;
+let showmeat = 0;
+let currentMonsters = [];
 let victories = 0;
 let defeats = 0;
 
@@ -22,69 +15,94 @@ const startScreen = document.getElementById('start-screen');
 const gameScreen = document.getElementById('game-screen');
 const resultScreen = document.getElementById('result-screen');
 const resultText = document.getElementById('result-text');
+const victoryCount = document.getElementById('victory-count');
+const pigMeat = document.getElementById('pig-meat');
 
-// スタートボタンのクリックイベント
 document.getElementById('start-button').addEventListener('click', () => {
   startScreen.style.display = 'none';
   gameScreen.style.display = 'block';
-  showRandomMonster();
+  showRandomMonsters();
 });
 
-// 戦闘ボタンのクリックイベント
-document.getElementById('attack-button').addEventListener('click', () => {
-  const random = Math.random();
+document.querySelectorAll('.attack-button').forEach(button => {
+  button.addEventListener('click', event => {
+    const choice = event.target.getAttribute('data-choice');
+    const random = Math.random();
 
-  if (random <= currentMonster.winRate) {
-    victories++;
-    defeats = 0;
-    resultText.textContent = '勝利！';
-    victories = 10;
+    if (random <= currentMonsters[choice - 1].winRate) {
+      victories++;
+      defeats = 0;
+      resultText.textContent = '勝利！';
+      victoryCount.textContent = `勝利数: ${victories}`;
       gameScreen.style.display = 'none';
       resultScreen.style.display = 'block';
       document.body.style.backgroundColor = '#00d0ff';
-      
+
       setTimeout(() => {
         startScreen.style.display = 'none';
         gameScreen.style.display = 'block';
-        showRandomMonster();
+        showRandomMonsters();
         document.body.style.backgroundColor = '';
         defeats = 0;
       }, 2000);
 
-  } else {
-    victories = 0;
-    defeats++;
-    resultText.textContent = '敗北...';
-
-    if (defeats >= 1) {
-      gameScreen.style.display = 'none';
-      resultScreen.style.display = 'block';
-      document.body.style.backgroundColor = 'black';
-      setTimeout(() => {
-        resultScreen.style.display = 'none';
-        startScreen.style.display = 'block';
-        document.body.style.backgroundColor = '';
-        defeats = 0;
-      }, 2000);
-      return;
+    } else {
+      victories = 0;
+      defeats++;
+      resultText.textContent = '敗北...';
+      if (defeats >= 1) {
+        gameScreen.style.display = 'none';
+        resultScreen.style.display = 'block';
+        document.body.style.backgroundColor = 'black';
+        setTimeout(() => {
+          resultScreen.style.display = 'none';
+          startScreen.style.display = 'block';
+          document.body.style.backgroundColor = '';
+          defeats = 0;
+        }, 2000);
+        return;
+      }
     }
-  }
 
-  gameScreen.style.display = 'none';
-  resultScreen.style.display = 'block';
+    gameScreen.style.display = 'none';
+    resultScreen.style.display = 'block';
 
-  if (victories >= 10) {
-    resultScreen.style.display = 'none';
-    document.getElementById('game-clear-screen').style.display = 'block';
-    console.log("価値")
-  }
+    if (victories >= 10) {
+      resultScreen.style.display = 'none';
+      document.getElementById('game-clear-screen').style.display = 'block';
+    }
+  });
 });
 
-function showRandomMonster() {
-  currentMonster = monsters[Math.floor(Math.random() * monsters.length)];
-  const monsterImage = document.getElementById('monster-image');
-  monsterImage.src = currentMonster.image;
-  monsterImage.alt = currentMonster.name;
-  document.getElementById('monster-name').textContent = currentMonster.name;
-  document.getElementById('win-rate').textContent = `勝率: ${currentMonster.winRate * 100}%`;
+function showRandomMonsters() {
+  currentMonsters = [];
+  const usedIndices = new Set();
+
+  for (let i = 0; i < 3; i++) {
+    let randomIndex;
+    do {
+      randomIndex = Math.floor(Math.random() * monsters.length);
+    } while (usedIndices.has(randomIndex));
+
+    usedIndices.add(randomIndex);
+    currentMonsters.push(monsters[randomIndex]);
+  }
+
+  for (let i = 0; i < 3; i++) {
+    const monsterImage = document.querySelectorAll('.monster-image')[i];
+    monsterImage.src = currentMonsters[i].image;
+    monsterImage.alt = currentMonsters[i].name;
+    document.querySelectorAll('.monster-name')[i].textContent = currentMonsters[i].name;
+    document.querySelectorAll('.win-rate')[i].textContent = `勝率: ${currentMonsters[i].winRate * 100}%`;
+  }
 }
+
+document.getElementById('pig-button').addEventListener('click', () => {
+  if (showmeat === 0) {
+    pigMeat.style.display = 'block';
+    showmeat = 1;
+  } else {
+    pigMeat.style.display = 'none';
+    showmeat = 0;
+  }
+});
