366e56ad769f35f6e0abbb216a95513d1f5555d3
ohaziki2 <haruya0014@gmail.com>
2023-08-20 15:20:33 +0900

    V2
    背景追加
    バランス調整

--- script.js
+++ script.js
@@ -101,6 +101,8 @@ document.querySelectorAll('.attack-button').forEach(button => {
         resultText.textContent = '敗北...';
         if (defeats >= 1) {
           invetory = []
+          nowScore = 0;
+          myScore.textContent = "スコア:" + 0;
           gameScreen.style.display = 'none';
           resultScreen.style.display = 'block';
           document.body.style.backgroundColor = 'black';
@@ -188,3 +190,60 @@ document.getElementById('pig-button').addEventListener('click', () => {
   
 });
 
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  function ground() {
+    var tl = gsap.timeline({
+      repeat: -1
+    });
+  
+    tl.to("#ground", 20, {
+      backgroundPosition: "1301px 0px",
+      force3D: true,
+      rotation: 0.01,
+      z: 0.01,
+      autoRound: false,
+      ease: Linear.easeNone
+    });
+  
+    return tl;
+  }
+  
+  function clouds() {
+    var tl = gsap.timeline({
+      repeat: -1
+    });
+  
+    tl.to("#clouds", 52, {
+      backgroundPosition: "-2247px bottom",
+      force3D: true,
+      rotation: 0.01,
+      z: 0.01,
+      //autoRound:false,
+      ease: Linear.easeNone
+    });
+  
+    return tl;
+  }
+
+  var masterTL = new TimelineMax({
+    repeat: -1
+  });
+  
+  // window load event makes sure image is 
+// loaded before running animation
+window.onload = function() {
+  var masterTL = gsap.timeline({
+    repeat: -1
+  });
+
+  masterTL
+    .add(ground(), 0)
+    .add(clouds(), 0)
+    .timeScale(0.7)
+    .progress(1)
+    .progress(0)
+    .play();
+};
+  
+});
